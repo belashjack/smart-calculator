7bba8e5a53443cd933f3bcf0588ab5d8d858fb14
Yauheni Belash <belashjack1@gmail.com>
2018-03-13 14:59:25 +0300

    Implement Smart Calculator class

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
-
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
-
-  multiply(number) {
-    // your implementation
-  }
-
-  devide(number) {
-    // your implementation
-  }
-
-  pow(number) {
-    // your implementation
-  }
+	constructor(initialValue) {
+		this.value = initialValue;
+	}
+	add(number) {
+		this.value += `+${number}`;
+		return this;
+	}
+	subtract(number) {
+		this.value += `-${number}`;
+		return this;
+	}
+	multiply(number) {
+		this.value += `*${number}`;
+		return this;
+	}
+	devide(number) {
+		this.value += `/${number}`;
+		return this;
+	}
+	pow(number) {
+		this.value += `**${number}`;
+		return this;
+	}
+	toString() {
+		return eval(this.value);
+	}
 }
 
 module.exports = SmartCalculator;
